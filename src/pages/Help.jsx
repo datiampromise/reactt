7ba6f8c92e2d01acf366a7f65970fe7141b9ce83
Promise Ezemokwe <promiseuzochukwu085@gmail.com>
2022-10-11 08:22:31 +0100

    added delet featres and help page

--- src/pages/Help.jsx
+++ src/pages/Help.jsx
@@ -1,10 +1,37 @@
 import React from "react";
+import ReactDOM from "react-dom/client";
 
 function Help() {
   return <h1>Help Page</h1>;
 }
 
 export default Help;
+
+function Faq(props) {
+  return (
+    <h5>
+      <li>I am a {props.question1}</li>
+    </h5>
+  );
+}
+
+function Answer() {
+  const faq = [
+    { id: 1, question1: "This is the answer to your question1" },
+    { id: 2, question2: "This is the answer to another question2" },
+    { id: 3, question3: "This is the answer to the third question3" },
+  ];
+  return (
+    <>
+      <h1>FAQ</h1>
+      <ul>
+        {faq.map((faq) => (
+          <Faq key={faq.id} question1={faq.question1} />
+        ))}
+      </ul>
+    </>
+  );
+}
 
 // Assignment
 // Create and loop through help informnation
