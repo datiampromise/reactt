68a9cddff641123c30c7c0e10f8e4050e0b223a6
Promise Ezemokwe <promiseuzochukwu085@gmail.com>
2022-10-14 08:09:28 +0100

    added help page

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,21 @@ import "./App.css";
 import About from "./pages/About";
 import Index from "./pages/Index";
 import Todo from "./pages/Todo";
+import Help from "./pages/Help";
 
 function App() {
   return (
     <>
       <header className="headerx">
         <nav>
-          <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/todo">Todo</Link>
+          <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/todo">Todo</Link> | <Link to="/help">Help</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/about" element={<About />} />
         <Route path="/todo" element={<Todo />} />
+        <Route path="/help" element={<Help />} />
       </Routes>
     </>
   );
