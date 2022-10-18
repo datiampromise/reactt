79ff3b565cc07561e454794fbe756a381f5c2a9e
Promise Ezemokwe <promiseuzochukwu085@gmail.com>
2022-10-18 08:30:01 +0100

    added new page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { Link, Route, Routes } from "react-router-dom";
+import "./index.css";
 
 import About from "./pages/About";
 import Index from "./pages/Index";
 import Todo from "./pages/Todo";
 import Help from "./pages/Help";
+import Sidebar from "./pages/Sidebar";
 
 function App() {
   return (
     <>
       <header className="headerx">
         <nav>
-          <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/todo">Todo</Link> | <Link to="/help">Help</Link>
+          <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/todo">Todo</Link> | <Link to="/help">Help</Link> |{" "}
+          <Link to="/sidebar">Sidebar</Link>
         </nav>
       </header>
       <Routes>
@@ -18,6 +21,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/todo" element={<Todo />} />
         <Route path="/help" element={<Help />} />
+        <Route path="/sidebar" element={<Sidebar />} />
       </Routes>
     </>
   );
